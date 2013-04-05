261428f3c783a3e434e70643863ce784f157e3bf
Christoph Deil <Deil.Christoph@gmail.com>
2013-04-05 17:15:14 +0200

    update test_csv.py

--- rootpy/tree/tests/test_csv.py
+++ rootpy/tree/tests/test_csv.py
@@ -1,9 +1,30 @@
-from rootpy.tree import Tree
-import rootpy.io
+import os
+from StringIO import StringIO
+#from rootpy.tree import Tree
+from rootpy.testdata import get_file
 
-f = rootpy.io.open('ttree.root')
-tree = f.get('ParTree_Postselect', ignore_unsupported=True)
-tree.csv()
+def test_read():
+	"""Test reading a TTree for a ROOT file"""
+	f = get_file('ttree.root')
+	tree = f.get('ParTree_Postselect')
+	# assert something!
 
-for event in tree:
-    print event
+def test_csv_write():
+	f = get_file('ttree.root')
+	tree = f.get('ParTree_Postselect')
+	print len(tree)
+	print tree.branchnames
+	tree.csv()
+	# read head and tail and assert OK!
+
+def test_looping():
+	f = get_file('ttree.root')
+	tree = f.get('ParTree_Postselect', ignore_unsupported=True)
+	n_events = 0
+	for event in tree:
+		n_events += 1
+	assert n_events == 5
+
+if __name__ == "__main__":
+    
+    test_csv_write()
